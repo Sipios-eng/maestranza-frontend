d1d788c6282d96082258b552bda52e2b4401d8f0
Sipios-eng <art.moya@duocuc.cl>
2025-06-23 00:13:31 -0400

    fix: Update API baseURL to PythonAnywhere deployment

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/', // ¡Asegúrate de que esta sea la URL base de tu backend!
+  baseURL: 'https://Sipios.pythonanywhere.com/', // ¡Asegúrate de que esta sea la URL base de tu backend!
 });
 
 api.interceptors.request.use(
